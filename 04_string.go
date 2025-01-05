5a9f9f742785347c7e393f20f70057d76f9eab78
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-01-05 17:08:59 +0800

    tipe data string

---
+++ 04_string.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Rizqi")
+  fmt.Println("Rizqi Nur")
+  fmt.Println("Rizqi Nur Assyaufi")
+
+  fmt.Println("Rizqi"[3])
+  fmt.Println("Rizqi Nur"[6])
+  fmt.Println("Rizqi Nur Assyaufi"[9])
+
+  fmt.Println(len("Rizqi"))
+  fmt.Println(len("Rizqi Nur"))
+  fmt.Println(len("Rizqi Nur Assyaufi"))
+}
