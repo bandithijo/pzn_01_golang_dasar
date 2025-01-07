2592531dc51b934e31b7362108ff71783ddbd25c
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-01-08 03:57:43 +0800

    function

---
+++ 19_function.go
@@ -0,0 +1,17 @@
+// Function
+// - Sebelumnya kita sudah mengenal sebuah function yang wajib dibuat agar program kita berjalan, yaitu function main
+// - Function adalah sebuah blok kode yang sengaja dibuat dalam program agar bisa digunakan berulang-ulang
+// - Cara membuat function sangat sederhana, hanya dengan menggunakan kata kunci func lalu diikuti dengan nama function nya dan blok kode function nya
+// - Setelah membuat function, kita bisa mengeksekusi function tersebut dengan memanggilnya menggunakan kata kunci nama function nya diikuti tanda kurung buka, kurung tutup
+
+package main
+
+import "fmt"
+
+func sayHello() {
+  fmt.Println("Hello, Go!")
+}
+
+func main() {
+  sayHello()
+}
