f6e1434fa4b7357e511616d261dd08f53424f20b
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-01-09 10:31:13 +0800

    comment

---
+++ 31_comment.go
@@ -0,0 +1,15 @@
+package main
+
+// Comment
+// - Komentar terbaik pada kode adalah kode itu sendiri
+// - Saat membuat kode, kita perlu membuat kode semudah mungkin untuk dibaca
+// - Namun kadang juga kita butuh menambahkan komentar di kode kita
+
+/*
+This is multiline comment
+You can add a lot of comments here
+*/
+
+func main() {
+  // this is single line comment
+}
